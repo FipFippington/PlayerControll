aad266a157294c835e947658e374e7a961bb410e
FipFippington <116121197+FipFippington@users.noreply.github.com>
2023-02-10 15:23:27 -0800

    Finished everything except bonus
    YIPPPPEEEE

--- PlayerControlValenzuela/Assets/Challenge 1/Scripts/SpinPropellerX.cs
+++ PlayerControlValenzuela/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class SpinPropellerX : MonoBehaviour
 {
-    public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +11,8 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        transform.position = player.transform.position + offset;
+        transform.Rotate(Vector3.up, 20);
     }
 }
